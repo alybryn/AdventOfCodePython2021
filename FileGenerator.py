d53ec9ffb7aa82cd200d5f679e9e164bed82c0c0
alybryn <alybryn@users.noreply.github.com>
2023-01-16 22:58:27 -0500

    Update FileGenerator.py

--- FileGenerator.py
+++ FileGenerator.py
@@ -1,9 +1,10 @@
+"""
 for i in (26):
-  contents = ""
-  with open("input%(i)s.txt", "w+") as file:
+  input_contents = ""
+  with open("input{}.txt".format(i), "w+") as file:
     file.write(str(contents))
-    """
-  contents = ""
-  with open("Day%(i)s.py", "w+") as file:
-    file.write(str(contents))
-    """
+
+  day_contents = "{}".format(i)
+  with open("Day{}.py".format(i), "w+") as file:
+    file.write(str(contents.format(i)))
+"""
